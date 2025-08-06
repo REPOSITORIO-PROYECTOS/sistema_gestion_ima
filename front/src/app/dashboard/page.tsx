1748395505edcda30be776ca1cf9b1e1f6cc0421
Valentino Privitera <devalen543@gmail.com>
2025-08-06 19:50:39 -0300

    Update Inicio App

--- front/src/app/dashboard/page.tsx
+++ front/src/app/dashboard/page.tsx
@@ -1,19 +1,20 @@
 "use client"
 
-// import { useEmpresaStore } from "@/lib/empresaStore";
+import { useEmpresaStore } from "@/lib/empresaStore";
 
 export default function Inicio() {
 
-  // const empresa = useEmpresaStore((state) => state.empresa);
-
-  // console.log(empresa)
+  const empresa = useEmpresaStore((state) => state.empresa);
+  console.log(empresa)
 
   return (
 
     /* Página Bienvenida */
     <div className="flex flex-col items-center gap-4">
 
-      <h1 className="text-3xl font-bold text-green-950 text-center">Sistema de Gestión Jugos Swing</h1>
+      <h1 className="text-3xl font-bold text-green-950 text-center">
+        {`Sistema de Gestión - ${empresa?.nombre_negocio}`}
+      </h1>
 
     </div>
   );
